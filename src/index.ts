1255fb20e17da5d4c8c55186d640d74cce411a46
tpluscode <tpluscode@users.noreply.github.com>
2023-02-10 14:09:05 +0100

    chore: remove test file and add exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
 export { findNodes } from './lib/findNodes'
 export { toSparql } from './lib/toSparql'
+export {
+  PathVisitor,
+  ShaclPropertyPath,
+  ZeroOrMorePath,
+  OneOrMorePath,
+  PredicatePath,
+  AlternativePath,
+  InversePath,
+  SequencePath,
+  ZeroOrOnePath,
+  fromNode,
+} from './lib/path'
