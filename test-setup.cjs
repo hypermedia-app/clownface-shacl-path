0244e5a80d9efb7504474dd6fdc03e49358afdf0
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2023-03-19 15:48:39 +0100

    test: ts-node loader

--- test-setup.cjs
+++
@@ -1,5 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires, import/no-extraneous-dependencies */
-require('@babel/register')({
-  configFile: './babel.config.json',
-  extensions: ['.js', '.jsx', '.ts', '.tsx'],
-})
